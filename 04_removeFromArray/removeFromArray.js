8ef64c1e080c51eee5c60df80d36ba48d1df1cb2
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-02-28 15:38:16 +0100

    Commented 04_removeFromArray for more clarity

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,6 +2,8 @@
 
 const removeFromArray = function(array, ...num) {
     
+    // filter out elements from 'array' that are also present in 'num'
+    // basically makes a new array that stores all elements that do not equal 'num'
     return array.filter(val => !num.includes(val))
     
 };
