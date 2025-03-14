c6cbdc775d6fdc2da66c22ba0390e528f4efb345
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-03-14 15:16:33 +0100

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const subtract = function() {
+const sum = function(arr) {
+  return arr.reduce((sum, val) => sum + val, 0);
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((sum, val) => sum * val)
 
 };
 
-const power = function() {
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
+
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let result = num1;
+    for (let i = 1; i < num1; i++){
+      result *= i;
+    }
+  return result === 0 ? 1 : result;
 };
 
 // Do not edit below this line
