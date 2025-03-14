5f693de31171c9ca61437ee21b0c7245724e21d0
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-03-14 15:39:43 +0100

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let lowerCase = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reverseWord = lowerCase.split('').reverse().join('');
 
+    return lowerCase === reverseWord ? true : false;
 };
 
 // Do not edit below this line
