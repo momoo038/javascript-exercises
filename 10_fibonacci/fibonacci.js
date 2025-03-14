31778be5fa30985ebb1db2cef1e62d0b85f5c6d5
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-03-14 16:27:23 +0100

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    
+    // base case: if num is 0 or 1, return num
+    if (num < 0) return "OOPS";
+    if (num == 0) return num;
+    if (num == 1) return num;
+ 
+    // fibonacci = Fn = Fn-1 + Fn-2 
+    return fibonacci(num - 1) + fibonacci(num - 2)
 };
 
 // Do not edit below this line
