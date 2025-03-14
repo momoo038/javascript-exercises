be2068f93bf890bad204a1ccdff5679f34cdcb6f
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-03-14 17:19:07 +0100

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const calcAge = people.sort((a, b) =>{
+        // initialize age variables
+        let aAge = 0;
+        let bAge = 0;
 
-};
-
+        // if yearOfDeath has a value, calculate total age
+        if (Number.isInteger(a.yearOfDeath)) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        // if yearOfDeath value is undefined, get current year
+        } else if (b.yearOfDeath == undefined) {
+            aAge = new Date().getFullYear() - a.yearOfBirth;
+        }
+        if (Number.isInteger(b.yearOfDeath)) {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        } else if (b.yearOfDeath == undefined) {
+            bAge = new Date().getFullYear() - b.yearOfBirth;
+        }
+        // sort by oldest to youngest
+        return bAge < aAge ? -1 : 1;
+    })
+    // return oldest person (person at index 0 in the array)
+    return calcAge[0];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
